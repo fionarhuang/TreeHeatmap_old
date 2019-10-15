a6f482cf5c881a08b923f58b18d7f18871ff7f2e
fionarhuang <ruizhuRH@gmail.com>
2019-10-15 17:09:13 +0200

    fix the mismatch between heatmap and tree when some branches are scaled

--- R/TreeHeatmap.R
+++ R/TreeHeatmap.R
@@ -20,6 +20,10 @@
 #'   \strong{hm_data}.
 #' @param column_split_gap A numeric value to specify the gap between the
 #'   columns of heatmap that are split.
+#' @param column_anno A named vector to specify labels that are used to
+#'   annotate columns of heatmap.
+#' @param column_anno_size A numeric value to specify the size of the annotation
+#'   bar
 #' @param column_anno_color A named vector to specify colors that are used to
 #'   annotate columns of heatmap.
 #' @param column_anno_gap A numeric value to specify the gap between the
@@ -47,13 +51,30 @@
 #' @param rownames_size A numeric value to specify the size of row names.
 #' @param rownames_hjust The hjust for row names: 0 (left aligned); 0.5
 #'   (centered); 1 (right aligned).
+#' @param show_title A logical value to specify whether the title should
+#'   be displayed. The default is FALSE.
+#' @param title_hm The title of heatmap
+#' @param title_fontface The fontface of the title. The default is "bold".
+#' @param title_color  The color of the title. The default is "black".
+#' @param title_size The size of the title. The default is 3.
+#' @param title_angle The angle of the title. The default is 0.
+#' @param title_offset_x A numeric value to shift title along x-axis. The
+#'   defaut is 0.
+#' @param title_offset_y A numeric value to shift title along y-axis. The
+#'   defaut is 2.
+#' @param title_hjust The hjust for title: 0 (left aligned); 0.5
+#'   (centered); 1 (right aligned). The default is 0.5
+#' @param cluster_column A logical value, TRUE or FALSE. It specifies whether
+#'   columns of the heatmap should be ordered by similarity. The default is
+#'   TRUE. This is ignored when \strong{column_order} is given.
 #' @importFrom TreeSummarizedExperiment transNode findOS
 #' @importFrom ggtree ggtree
 #' @importFrom tidyr gather
 #' @importFrom dplyr mutate select distinct "%>%"
 #' @importFrom ggplot2 geom_tile geom_segment scale_color_manual labs geom_text scale_fill_viridis_c aes
 #' @importFrom ggnewscale new_scale_color
-#' @importFrom viridis  viridis
+#' @importFrom viridis viridis
+#' @importFrom stats hclust dist
 #' @export
 #' @author Ruizhu Huang
 #' @examples
@@ -61,13 +82,19 @@
 #' library(TreeSummarizedExperiment)
 #' library(ggtree)
 #' library(ggplot2)
+#' library(scales)
 #'
 #' data(tinyTree)
-#'
-#' ct <- matrix(rpois(n = 28, lambda = 10), ncol = 4)
-#' colnames(ct) <- paste("S", 1:4, sep = "")
+#' p1 <- c(0.1, 0.4, rep(0.1, 5))
+#' p2 <- c(0.4, 0.1, rep(0.1, 5))
+#' set.seed(1)
+#' ct <- cbind(rmultinom(n = 5, size = 50, prob = p1),
+#'             rmultinom(n = 5, size =50, prob = p2))
+#' colnames(ct) <- paste("S", 1:10, sep = "")
 #' rownames(ct) <- transNode(tree = tinyTree, node = c(13, 18, 6:10))
 #'
+#' oo <- sample(1:10)
+#' ct <- ct[, oo]
 #' # prepare the tree figure
 #' tree_fig <- ggtree(tinyTree,
 #'                    branch.length = "none",
@@ -75,28 +102,73 @@
 #'     #geom_text2(aes(label = label)) +
 #'     geom_hilight(node = 18, fill = "orange", alpha = 0.3) +
 #'     geom_hilight(node = 13, fill = "blue", alpha = 0.3)
+#' # figure 1
+#' TreeHeatmap(tree = tinyTree, tree_fig = tree_fig, hm_data = ct)
 #'
-#' col_split <- c("S1" = "C", "S2" = "B", "S3" = "A", "S4" = "A")
-#' col_anno <- c("S1" = "C", "S2" = "C", "S3" = "D", "S4" = "D")
-#' col_order <- rev(colnames(hm_data))
+#' # figure 2: order column by similarity
+#' TreeHeatmap(tree = tinyTree, tree_fig = tree_fig,
+#'             hm_data = ct, cluster_column = TRUE)
 #'
+#'  # figure 3: split columns
+#' col_split <- ifelse(colnames(ct) %in% paste0("S", 1:5),
+#'                     "A", "B")
+#' names(col_split) <- colnames(ct)
+#' TreeHeatmap(tree = tinyTree, tree_fig = tree_fig,
+#'             hm_data = ct, cluster_column = TRUE,
+#'             column_split = col_split)
+#' # figure 4: annotate columns
+#' col_anno <- col_split
+#' TreeHeatmap(tree = tinyTree, tree_fig = tree_fig,
+#'             hm_data = ct, cluster_column = TRUE,
+#'             column_split = col_split,
+#'             column_anno = col_anno,
+#'             column_anno_gap = 0.5)
+#' # figure 5: change annotation colors
+#' TreeHeatmap(tree = tinyTree, tree_fig = tree_fig,
+#'             hm_data = ct, cluster_column = TRUE,
+#'             column_split = col_split,
+#'             column_anno = col_anno,
+#'             column_anno_gap = 0.6,
+#'             column_anno_color = c("A" = "red", "B"= "blue"))
 #'
-#' fig <- TreeHeatmap(tree = tinyTree, tree_fig = tree_fig,
-#'                    hm_data = ct,
-#'                    column_anno = col_anno,
-#'                    column_anno_size = 1,
-#'                    column_anno_gap = 0.8,
-#'                    column_anno_color = c("C" = "red", "D" = "blue"),
-#'                    column_order = col_order,
-#'                    column_split = col_split,
-#'                    show_colnames = TRUE,
-#'                    show_rownames = TRUE,
-#'                    rownames_offset_x = 0.1, rownames_hjust = 0)
+#' # figure 6: add colnames
+#' TreeHeatmap(tree = tinyTree, tree_fig = tree_fig,
+#'             hm_data = ct, cluster_column = TRUE,
+#'             column_split = col_split,
+#'             column_anno = col_anno,
+#'             column_anno_gap = 0.6,
+#'             column_anno_color = c("A" = "red", "B"= "blue"),
+#'             show_colnames = TRUE,
+#'             colnames_position = "bottom",
+#'             colnames_angle = 90, colnames_size = 2,
+#'             colnames_offset_y = -0.2)
+#'
+#' # figure 7: add title
+#'  fig <- TreeHeatmap(tree = tinyTree, tree_fig = tree_fig,
+#'             hm_data = ct, cluster_column = TRUE,
+#'             column_split = col_split,
+#'             column_anno = col_anno,
+#'             column_anno_gap = 0.6,
+#'             column_anno_color = c("A" = "red", "B"= "blue"),
+#'             show_colnames = TRUE,
+#'             colnames_position = "bottom",
+#'             colnames_angle = 90, colnames_size = 2,
+#'             colnames_offset_y = -0.2,
+#'             show_title = TRUE,
+#'             title_offset_y = 1.5,
+#'             title_color = "blue")
 #' fig
 #' # use expand_limits to display the missing part of row names
-#' fig + expand_limits(x = c(0, 15))
+#' (fig <- fig + expand_limits(x = c(0, 15)))
 #'
-
+#' # change colors
+#' fig +
+#' scale_fill_viridis_c(option = "D")
+#'
+#' fig +
+#'  scale_fill_gradientn(colours = c("blue","yellow","red"),
+#'                       values = scales::rescale(c(5, 8, 10)),
+#'                       guide = "colorbar", limits=c(5, 10))
 
 TreeHeatmap <- function(tree, tree_fig, hm_data,
                         tree_hm_gap = 0,
@@ -122,7 +194,17 @@ TreeHeatmap <- function(tree, tree_fig, hm_data,
                         rownames_offset_x = 0,
                         rownames_offset_y = 0,
                         rownames_size = 4,
-                        rownames_hjust = 0.5){
+                        rownames_hjust = 0.5,
+                        show_title = FALSE,
+                        title_hm = "First heatmap",
+                        title_fontface = "bold",
+                        title_color = "black",
+                        title_size = 3,
+                        title_angle = 0,
+                        title_offset_x = 0,
+                        title_offset_y = 2,
+                        title_hjust = 0.5,
+                        cluster_column = FALSE){
 
 
     if (!is.null(column_order)) {
@@ -148,6 +230,7 @@ TreeHeatmap <- function(tree, tree_fig, hm_data,
     }
 
     ## tree: data
+
     df <- tree_fig$data
 
     # ------------------------ heatmap -----------------------
@@ -176,8 +259,14 @@ TreeHeatmap <- function(tree, tree_fig, hm_data,
         xx <- match(x, df$node)
         y <- df$y[xx]
 
+        cy <- colnames(df)
+        if ("scale" %in% cy) {
+            dt <- unique(df$scale[xx])
+        } else {
+            dt <- 1
+        }
         # the distance
-        diff(range(y, na.rm = TRUE)) + 1
+        diff(range(y, na.rm = TRUE)) + dt
     })
     hm_df$height <- unlist(h_hm)
 
@@ -194,22 +283,56 @@ TreeHeatmap <- function(tree, tree_fig, hm_data,
                     -c(y, node, row_label,
                        width, height))
 
-    # heatmap: x
+    # heatmap: column order
     if (!is.null(column_split)) {
+        # 1. column_split is given, ignore column order. The order within the
+        #    same slice is determined by the input order of column split
+        # 2. column_split is given and column_cluster = TRUE, the order within
+        #    the same slice is determined by the column similarity
         column_split <- factor(column_split, levels = unique(column_split))
+        if (cluster_column) {
+            # column similarity within the same slice
+            ind_split <- lapply(levels(column_split), FUN = function(x){
+                names(column_split)[column_split == x]
+            })
+            similar <- lapply(ind_split, FUN = function(x) {
+                if (length(x) > 2) {
+                    st <- hm_data[, x, drop = FALSE]
+                    xx <- hclust(dist(t(st)), "ave")
+                    colnames(st)[xx$order]
+                } else { x }
+            })
+            similar_order <- unlist(similar)
+            column_split <- column_split[similar_order]
+        }
         split_level <- sort(column_split)
 
         if (!is.null(column_order)) {
             warnings("column_order is ignored when column_split is given")}
         column_order <- names(split_level)
     } else {
+        ## column_split isn't given
+        # 1. column_order is given, use column_order and ignore column
+        #    similarity.
+        # 2. column_order isn't given but allow cluster columns, order columns
+        #    by similarity
+        # 3. column_order isn't given and cluter columns is not allowed, use the
+        #    original column order.
+
         split_level <- rep(0, ncol(hm_data))
         names(split_level) <- colnames(hm_data)
         split_level <- factor(split_level)
-        if (is.null(column_order)) {
+        if (is.null(column_order) & !cluster_column) {
             column_order <- colnames(hm_data)}
+        if (is.null(column_order) & cluster_column) {
+            hc <- hclust(dist(t(hm_data)), "ave")
+            column_order <- colnames(hm_data)[hc$order]}
+        if (!is.null(column_order) & cluster_column) {
+            warnings("cluster_column is ignored because column_order is given")
+        }
     }
 
+    # heatmap: x
     hm_dt <- hm_dt %>%
         mutate(variable = factor(variable, levels = column_order)) %>%
         mutate(column_order = as.numeric(variable) -1) %>%
@@ -312,5 +435,23 @@ TreeHeatmap <- function(tree, tree_fig, hm_data,
                            hjust = rownames_hjust)
     }
 
+    # # -------------------- heatmap title ----------------
+    if (show_title) {
+        label <- NULL
+        title_df <- data.frame(x = mean(range(hm_dt$x, na.rm = TRUE)),
+                               y = max(hm_dt$y),
+                               label = title_hm)
+        p <- p + geom_text(data = title_df,
+                           aes(x = x, y = y,
+                               label = label),
+                           inherit.aes = FALSE,
+                           fontface = title_fontface,
+                           colour = title_color,
+                           size = title_size,
+                           angle = title_angle,
+                           nudge_x = title_offset_x,
+                           nudge_y = title_offset_y,
+                           hjust = title_hjust)
+    }
     return(p)
 }
